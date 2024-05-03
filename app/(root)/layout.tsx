dcf1c7951ee9789d08fec23739dcac502d0f6c69
tejavi8686 <tejasviraj8686@gmail.com>
2024-05-03 16:08:37 +0530

    metting room done!

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import { StreamVideoProvider } from "@/providers/StreamClientProviders";
+import { Metadata } from "next";
 import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
