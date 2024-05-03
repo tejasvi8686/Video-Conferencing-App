dca595d67b8b9da425c7cfa6a235de49337adf07
tejavi8686 <tejasviraj8686@gmail.com>
2024-05-03 18:05:07 +0530

    join meeting model done

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -9,6 +9,7 @@ import { useUser } from "@clerk/nextjs";
 import { Description } from "@radix-ui/react-dialog";
 import {  useToast } from "./ui/use-toast";
 import { Textarea } from "./ui/textarea";
+import { Input } from "./ui/input";
 
 const MeetingTypeList = () => {
   const { toast } = useToast()
@@ -144,6 +145,30 @@ const MeetingTypeList = () => {
           buttonText="Copy Meeting Link"
         />
       )}
+
+<MeetingModal
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder="Meeting link"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+      </MeetingModal>
+
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
     </section>
   );
 };
